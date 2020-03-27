fbc09799e0c063c2c87550d89621f9e9584f196c
jecsham <jecsham27@gmail.com>
2020-03-27 13:19:24 -0500

    forge config fixes

--- forge.config.js
+++ forge.config.js
@@ -1,34 +1,35 @@
 const path = require('path');
 module.exports = {
   packagerConfig: {
-      icon: path.resolve(__dirname, 'src/assets/icons/icon'),
-      asar: true,
-      executableName: "My System Information"
+    icon: path.resolve(__dirname, 'src/assets/icons/icon'),
+    asar: true,
+    // executableName: "My System Information",
   },
-  plugins: 
-  [
+  plugins:
     [
-      "@electron-forge/plugin-webpack",
-      {
-        mainConfig: path.resolve(__dirname, './webpack.main.config.js'),
-        renderer: {
-          config: path.resolve(__dirname, './webpack.renderer.config.js'),
-          entryPoints: [
-            {
-              html: path.resolve(__dirname, './src/views/main/main.view.html'),
-              js: path.resolve(__dirname, './src/views/main/main.view.js'),
-              name: "main_window"
-            }
-          ]
+      [
+        "@electron-forge/plugin-webpack",
+        {
+          mainConfig: path.resolve(__dirname, './webpack.main.config.js'),
+          renderer: {
+            config: path.resolve(__dirname, './webpack.renderer.config.js'),
+            entryPoints: [
+              {
+                html: path.resolve(__dirname, './src/views/main/main.view.html'),
+                js: path.resolve(__dirname, './src/views/main/main.view.js'),
+                name: "main_window"
+              }
+            ]
+          }
         }
-      }
-    ]
-  ],
+      ]
+    ],
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
       config: {
-        name: "my_system_information"
+        name: "my-system-information",
+        loadingGif: path.resolve(__dirname, 'src/assets/gif/installing.gif'),
       }
     },
     {
@@ -44,6 +45,17 @@ module.exports = {
     {
       name: "@electron-forge/maker-rpm",
       config: {}
+    }
+  ],
+  publisher: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          name: 'my-system-information'
+        },
+        prerelease: false
+      }
     }
   ]
 }
